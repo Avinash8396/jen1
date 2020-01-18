45352bca506af3027cdf078a353cfdccfa428b92
Sharma <836992@cognizant.com>
2020-01-18 15:16:21 +0530

    Welcome to jenkins project.

--- cl.java.java
+++ cl.java.java
@@ -1,5 +1,5 @@
 class Helloworld{  
     public static void main(String args[]){  
-     System.out.println("Project Kub Milega???");  
+     System.out.println("New jenkin project");  
     }  
 }  
